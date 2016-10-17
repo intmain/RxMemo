2d994c3db80690e1ca8615c15005ee054fc76557
Leonard <leonard.happy@kakaocorp.com>
2016-10-17 23:47:48 +0900

    bindTo로 변경.

--- Memo/ViewController.swift
+++ Memo/ViewController.swift
@@ -14,30 +14,27 @@ import RxDataSources
 class ViewController: UIViewController {
     
     let datasource = ["memo1", "memo2", "memo3","memo4", "memo5", "memo6","memo7", "memo8", "memo9"]
-
+    let disposeBag = DisposeBag()
     @IBOutlet var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        bindDataSource()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
 }
 
-extension ViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return datasource.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemoCell", for: indexPath) as? MemoCell else { return MemoCell() }
-        
-        cell.memoLabel.text = datasource[indexPath.row]
-        return cell
+
+// MARK: - Rx
+extension ViewController {
+    func bindDataSource() {
+        Observable.of(datasource).bindTo(collectionView.rx.items(cellIdentifier: "MemoCell", cellType: MemoCell.self)) {  index, item, cell in
+            cell.memoLabel.text = "\(index+1): item"
+        }.addDisposableTo(disposeBag)
     }
 }
