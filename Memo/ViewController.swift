098273564bb697eeee9b1aac1f020ae75198a382
Leonard <leonard.happy@kakaocorp.com>
2016-10-17 23:57:38 +0900

    bindTo 없이 구현.

--- Memo/ViewController.swift
+++ Memo/ViewController.swift
@@ -33,8 +33,35 @@ class ViewController: UIViewController {
 // MARK: - Rx
 extension ViewController {
     func bindDataSource() {
-        Observable.of(datasource).bindTo(collectionView.rx.items(cellIdentifier: "MemoCell", cellType: MemoCell.self)) {  index, item, cell in
+//        Observable.of(datasource).bindTo(collectionView.rx.items(cellIdentifier: "MemoCell", cellType: MemoCell.self)) {  index, item, cell in
+//            cell.memoLabel.text = "\(index+1): item"
+//        }.addDisposableTo(disposeBag)
+        
+        collectionView.rx.items(cellIdentifier: "MemoCell", cellType: MemoCell.self)(Observable.of(datasource))( {index, item, cell in
             cell.memoLabel.text = "\(index+1): item"
-        }.addDisposableTo(disposeBag)
+        }).addDisposableTo(disposeBag)
+        
+// public func bindTo<R1, R2>(_ binder: (Self) -> @escaping (R1) -> R2, curriedArgument: R1) -> R2
     }
 }
+
+/**
+ public func items<S: Sequence, Cell: UICollectionViewCell, O : ObservableType>
+ (cellIdentifier: String, cellType: Cell.Type = Cell.self)
+ -> (_ source: O)
+ -> (_ configureCell: @escaping (Int, S.Iterator.Element, Cell) -> Void)
+ -> Disposable where O.E == S {
+ return { source in
+ return { configureCell in
+ let dataSource = RxCollectionViewReactiveArrayDataSourceSequenceWrapper<S> { (cv, i, item) in
+ let indexPath = IndexPath(item: i, section: 0)
+ let cell = cv.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! Cell
+ configureCell(i, item, cell)
+ return cell
+ }
+ 
+ return self.items(dataSource: dataSource)(source)
+ }
+ }
+ }
+ **/
